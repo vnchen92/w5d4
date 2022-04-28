dc9bcc7d716e0b7848558e7bb038bb852b1fd6c3
Tom Li <tomleslieli@yahoo.com>
2022-04-28 14:33:26 -0400

    Up to Course in Associations Exercise

--- 01_Associations_Exercise/app/models/course.rb
+++ 01_Associations_Exercise/app/models/course.rb
@@ -1,2 +1,27 @@
+# == Schema Information
+#
+# Table name: courses
+#
+#  id            :bigint           not null, primary key
+#  name          :string
+#  prereq_id     :integer
+#  instructor_id :integer
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
 class Course < ApplicationRecord
+
+    #connecting enrollment to course
+    has_many :enrollments,
+    primary_key: :id,
+    foreign_key: :course_id,
+    class_name: :Enrollment
+
+    #connecting user to course
+    has_many :enrolled_students,
+    through: :enrollments,
+    source: :user
+
+    belongs_to :
+
 end
--- 01_Associations_Exercise/app/models/user.rb
+++ 01_Associations_Exercise/app/models/user.rb
@@ -1,2 +1,22 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class User < ApplicationRecord
+
+    #connecting user to enrollment
+    has_many :enrollments,
+    primary_key: :id,
+    foreign_key: :student_id,
+    class_name: :Enrollment
+
+    #connecting user to course
+    has_many :enrolled_courses,
+    through: :enrollments,
+    source: :course
 end
